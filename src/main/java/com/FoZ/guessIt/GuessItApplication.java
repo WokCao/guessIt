a1b31313c0a11e2314199e3e9d8d31c67085c5f2
WokCao <henryquoccao@gmail.com>
2025-08-01 20:00:58 +0700

    Init base code

---
+++ src/main/java/com/FoZ/guessIt/GuessItApplication.java
@@ -0,0 +1,13 @@
+package com.FoZ.guessIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FoZ/guessIt/GuessItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoZ.guessIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
